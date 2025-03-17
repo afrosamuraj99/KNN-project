e6dc94003556509350c785d37959c0dd763c896a
Filip Banak <xbanak01@stud.fit.vutbr.cz>
2025-03-17 22:03:47 +0100

    ColorizeNet: correct colorization testing script

--- ColorizeNet/colorize.py
+++ ColorizeNet/colorize.py
@@ -1,3 +1,7 @@
+from safe_gpu import safe_gpu
+
+safe_gpu.claim_gpus()
+
 import random
 
 import cv2
@@ -11,13 +15,11 @@ from utils.ddim import DDIMSampler
 from utils.model import create_model, load_state_dict
 
 model = create_model('./models/cldm_v21.yaml').cpu()
-model.load_state_dict(load_state_dict(
-    'lightning_logs/version_6/checkpoints/colorizenet-sd21.ckpt', location='cuda'))
+model.load_state_dict(load_state_dict('./checkpoints/colorizenet-sd21.ckpt', location='cuda'))
 model = model.cuda()
 ddim_sampler = DDIMSampler(model)
 
-
-input_image = cv2.imread("sample_data/sample1_bw.jpg")
+input_image = cv2.imread("test_imgs/cat.jpg")
 input_image = HWC3(input_image)
 img = resize_image(input_image, resolution=512)
 H, W, C = img.shape
@@ -26,7 +28,6 @@ num_samples = 1
 control = torch.from_numpy(img.copy()).float().cuda() / 255.0
 control = torch.stack([control for _ in range(num_samples)], dim=0)
 control = einops.rearrange(control, 'b h w c -> b c h w').clone()
-
 
 # seed = random.randint(0, 65535)
 seed = 1294574436
@@ -39,14 +40,11 @@ eta = 0.0
 ddim_steps = 20
 scale = 9.0
 
-cond = {"c_concat": [control], "c_crossattn": [
-    model.get_learned_conditioning([prompt] * num_samples)]}
-un_cond = {"c_concat": None if guess_mode else [control], "c_crossattn": [
-    model.get_learned_conditioning([n_prompt] * num_samples)]}
+cond = {"c_concat": [control], "c_crossattn": [model.get_learned_conditioning([prompt] * num_samples)]}
+un_cond = {"c_concat": None if guess_mode else [control], "c_crossattn": [model.get_learned_conditioning([n_prompt] * num_samples)]}
 shape = (4, H // 8, W // 8)
 
-model.control_scales = [strength * (0.825 ** float(12 - i)) for i in range(13)] if guess_mode else (
-    [strength] * 13)
+model.control_scales = [strength * (0.825 ** float(12 - i)) for i in range(13)] if guess_mode else ([strength] * 13)
 samples, intermediates = ddim_sampler.sample(ddim_steps, num_samples,
                                              shape, cond, verbose=False, eta=eta,
                                              unconditional_guidance_scale=scale,
